12dd6eb7512abaea13dfbb15b2f728fe303edc32
Eugene <Fl1p92@ukr.net>
2018-12-11 16:43:04 +0200

    Tutorial Part 5.

--- rest/settings.py
+++ rest/settings.py
@@ -116,3 +116,8 @@ STATIC_ROOT = BASE_DIR('public/static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR('public/media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
